80284190cff77332d2b61c49bde4011e714d3a81
Marek Matušica <83821425+mmm-marek@users.noreply.github.com>
2022-04-21 23:01:51 +0200

    Refactor: HomePage switch removed with simple if statement

---
+++ src/Components/InfoPanel/InfoPanel.component.tsx
@@ -0,0 +1,8 @@
+const InfoPanel = () => {
+    return (
+        <div>
+            <h1>INFO PANEL</h1>
+        </div>
+    );
+};
+export default InfoPanel;
--- src/Pages/HomePage/HomePage.component.tsx
+++ src/Pages/HomePage/HomePage.component.tsx
@@ -1,32 +1,27 @@
 import { useContext } from "react";
 import ChooseSearchMethodForm from "../../Components/ChooseSearchMethodForm/ChooseSearchMethodForm.component";
+import InfoPanel from "../../Components/InfoPanel/InfoPanel.component";
 import SearchByCityForm from "../../Components/SearchByCityForm/SearchByCityForm.component";
 import SearchByCoordinatesForm from "../../Components/SearchByCoordinatesForm/SearchByCoordinatesForm.component";
 import { UserDataContext } from "../../Context/userData.context";
 import SearchBy from "../../Enums/searchBy.enum";
 
 const HomePage = () => {
-    const { searchBy } = useContext(UserDataContext);
+    const { searchBy, searchHistoryData } = useContext(UserDataContext);
 
-    switch (searchBy) {
-        case SearchBy.Coordinates: {
-            return (
-                <div>
-                    <SearchByCoordinatesForm />
-                </div>
-            );
-        }
-        case SearchBy.City: {
-            return (
-                <div>
-                    <SearchByCityForm />
-                </div>
-            );
-        }
-        default: {
-            return <ChooseSearchMethodForm />;
-        }
+    if (searchBy === SearchBy.NotChosen) {
+        return <ChooseSearchMethodForm />;
     }
+    return (
+        <div>
+            {searchBy === SearchBy.City ? (
+                <SearchByCityForm />
+            ) : (
+                <SearchByCoordinatesForm />
+            )}
+            {searchHistoryData.length > 0 && <InfoPanel />}
+        </div>
+    );
 };
 
 export default HomePage;
