20380568122ea9c7d368c751e803e6249a49b225
Marek Matušica <83821425+mmm-marek@users.noreply.github.com>
2022-05-05 17:55:39 +0200

    feat: Data table added and styled in history page

---
+++ src/Components/DataTable/DataTable.component.tsx
@@ -0,0 +1,65 @@
+import { useContext } from "react";
+import { UserDataContext } from "../../Context/userData.context";
+import TypeOfUser from "../../Enums/typeOfUser.enum";
+import { WeatherData } from "../../Types/userData.types";
+import "./DataTable.styles.scss";
+
+const TableHeader = () => {
+    const { typeOfUser } = useContext(UserDataContext);
+    return (
+        <div className="table-header table-line-display">
+            <span>PLACE</span>
+            <span>TEMPERATURE</span>
+            <span>PRESSURE</span>
+            <span>HUMIDITY</span>
+            <span>WIND SPEED</span>
+            {typeOfUser === TypeOfUser.Meteorologist && (
+                <>
+                    <span>WIND DIRECTION</span>
+                    <span>MAX TEMPERATURE</span>
+                    <span>MIN TEMPERATURE</span>
+                    <span>LATITUDE</span>
+                    <span>LONGITUDE</span>
+                </>
+            )}
+        </div>
+    );
+};
+
+const DataRow = (props: WeatherData) => {
+    const { typeOfUser } = useContext(UserDataContext);
+    return (
+        <div className="table-line table-line-display">
+            <span>{props.place.city}</span>
+            <span>{props.temperature} °C</span>
+            <span>{props.pressure}</span>
+            <span>{props.humidity} %</span>
+            <span>{props.windspeed} km/h</span>
+            {typeOfUser === TypeOfUser.Meteorologist && (
+                <>
+                    <span>{props.windDirection}</span>
+                    <span>{props.maxTemperature}</span>
+                    <span>{props.minTemperature}</span>
+                    <span>{props.place.latitude}</span>
+                    <span>{props.place.longitude}</span>
+                </>
+            )}
+        </div>
+    );
+};
+
+const DataTable = () => {
+    const { searchHistoryData } = useContext(UserDataContext);
+    return (
+        <div className="data-table-container">
+            <TableHeader />
+            <hr />
+            <div className="table-content">
+                {searchHistoryData.map((el) => (
+                    <DataRow {...el} />
+                ))}
+            </div>
+        </div>
+    );
+};
+export default DataTable;
--- src/Pages/HistoryPage/HistoryPage.component.tsx
+++ src/Pages/HistoryPage/HistoryPage.component.tsx
@@ -1,15 +1,10 @@
-import { useContext } from "react";
-import InfoPanel from "../../Components/InfoPanel/InfoPanel.component";
-import { UserDataContext } from "../../Context/userData.context";
+import DataTable from "../../Components/DataTable/DataTable.component";
+import "./HistoryPage.styles.scss";
 
 const HistoryPage = () => {
-    const { searchHistoryData } = useContext(UserDataContext);
-
     return (
-        <div>
-            {searchHistoryData.map((el) => (
-                <InfoPanel dataToDisplay={el} key={el.id} />
-            ))}
+        <div className="historypage-container">
+            <DataTable />
         </div>
     );
 };
